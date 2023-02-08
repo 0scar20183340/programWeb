c56ee48d780c7567c2ce64eeb263f6a56d5df4f8
0scar20183340 <90484177+0scar20183340@users.noreply.github.com>
2023-02-08 16:59:23 -0600

    Update practica1.js

--- practica1.js
+++ practica1.js
@@ -1,7 +1,7 @@
 //npm init -y crea package.json
 //package.json paquetes de dependencias instalados 
 
-
+//Con .gitignore seleccionamos archivos que no queremos subir al repositorio
 
 
 const { toCelsius } = require ("celsius");    
@@ -21,11 +21,10 @@ const { toFahrenheit } = require ("celsius") ;
 console.log("F A C")
 console.log(toFahrenheit(36)); //  97 
 console.log(toFahrenheit(36.68 , 3 )); //  98.024 
-// toFahrenheit ( ' -40.691 grados C ' , 5 )  ; //  -41.24380 
+
 
 console.log("PRUEBA DOBLE")
 const convertidor = require("celsius");
-//Asi uso la dependencia completa
+//Asi uso el paquete de dependencia completo
 console.log(convertidor.toCelsius(100));
 console.log(convertidor.toFahrenheit(36));
-
