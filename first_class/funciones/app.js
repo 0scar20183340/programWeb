b89b467310a0f13febf52ed3bc20e68aaec83ea2
Oscar <ogonzalez@ucol.mx>
2023-02-09 20:21:46 -0600

    refactorizacion

--- first_class/funciones/app.js
+++ first_class/funciones/app.js
@@ -1,1 +1,28 @@
 
+// function greet (){
+//     console.log("Hello");
+// }
+// greet();
+
+// function logGreenting(fn){
+//     fn();
+// };
+// logGreenting(greet);
+
+
+// let greetMe = function (){
+//     console.log("Hello from the function expression");
+// }
+// greetMe();
+// logGreenting(greetMe);
+
+
+//REFACTORIZACION DE FUNCTION CALLBACK
+let greet = () => console.log("Hola Mundo");
+let logGreenting = fn => fn();
+let greetMe = ()=> console.log("Hello desde la expresion de la funcion");
+
+greet();
+logGreenting(greet);
+greetMe();
+logGreenting(greetMe);
