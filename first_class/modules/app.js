be7274f6886123d10cbe544b1f1612473dab3462
Oscar <ogonzalez@ucol.mx>
2023-02-15 08:17:11 -0600

    objets

--- first_class/modules/app.js
+++ first_class/modules/app.js
@@ -11,38 +11,3 @@ greet("Hola mundo");
 //no se puede modificar
 
 
-let obj = {};
-let person = {
-    nombre : "carlos",
-    apellido: "Fonceca",
-
-    getName: function (){
-        return `${this.nombre} ${this.apellido}`
-        // llama a la isntancia del objeto person
-    }, //solo lectura
-
-    domicilio: {
-        calle: "Madero #222",
-        colonia: "Union",
-        cp: 29980,
-        minicipio: "Colima",
-
-        getDomicilio: function (){
-            //this manda a llamar la instancia 
-            // person.domicilio   llama al nombre de la instancia
-
-            // /utilzar this ES BUENA PRACTICA
-            return `Calle: ${this.calle} Colonia: ${this.colonia} Centro CP:${this.cp}, ${this.minicipio}`
-            // llama a la instancia del objeto domicilio ={}
-        }
-    }
-}
-
-console.log(person.getName());
-person.nombre = "Oscar"
-person.apellido = "Gonzalez"
-
-console.log(person.domicilio.getDomicilio());
-// console.log(`nombre: ${person.nombre} y apellido: ${person.apellido}`);
-//objeto JSON = objetsLiteral
-// key value = valores prinitos, funciones, objetos
